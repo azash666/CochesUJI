b582749b2062b46b80abfd3a993e52f3d76e0182
azash666 <ddelgadogomis@hotmail.com>
2018-10-26 12:54:48 +0200

    Creados los scripts

---
+++ CochesUJI/Assets/Scripts/AlgoritmosGeneticos.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlgoritmosGeneticos {
+    //TODO: Cambiar nombre de la clase
+    
+    CodigoADN[] individuos;
+
+
+    public void crearMuestraInicial(int n)
+    {
+
+    }
+
+    public void poda(int porcentaje)
+    {
+        //Selecciona los mejores
+    }
+
+    public void generarDescendientes()
+    {
+        //Genera los descendientes
+    }
+
+    public void combinar()
+    {
+        //Combinar los individuos
+    }
+
+    public void mutar()
+    {
+        //Mutar los individuos
+    }
+}
---
+++ CochesUJI/Assets/Scripts/CodigoADN.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodigoADN {
+    private int[] adn;
+
+    public CodigoADN()
+    {
+        //Crear un nuevo individuo aleatorio
+    }
+
+    public static CodigoADN combinar(CodigoADN adnA, CodigoADN adnB)
+    {
+        return null;
+    }
+
+    public void mutar()
+    {
+
+    }
+	
+}
