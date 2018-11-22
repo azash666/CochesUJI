19390705553bc029b26c437deceecd496e5d38bd
azash666 <ddelgadogomis@hotmail.com>
2018-11-22 19:36:34 +0100

    Unit testing de unity
    No consigo que me importe la clase

---
+++ CochesUJI/Assets/Scripts/Tests/DNACodeTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class DNACodeTest {
+
+    [Test]
+    public void DNACodeTestSimplePasses() {
+        // Use the Assert class to test conditions.
+        DNACode individuo = new DNACode();
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void otro()
+    {
+        
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator DNACodeTestWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
