1b23ee3a1587dfc123b550338c9c02a395d1fc41
arturo101199 <44496114+arturo101199@users.noreply.github.com>
2018-11-23 10:15:49 +0100

    MovimientoIA

---
+++ CochesUJI/Assets/Scripts/WP_Actor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WP_Actor : MonoBehaviour {
+    float speed = 5.0f;
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Waypoint")
+            target = other.gameObject.GetComponent<Waypoint>.NextPoint;
+    }
+}
