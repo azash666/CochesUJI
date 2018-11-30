0fb4c393b4d02e787cab3f58226ae30489e767f8
arturo101199 <44496114+arturo101199@users.noreply.github.com>
2018-11-30 12:19:48 +0100

    Update WP_Actor.cs

--- CochesUJI/Assets/Scripts/WP_Actor.cs
+++ CochesUJI/Assets/Scripts/WP_Actor.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class WP_Actor : Movement {
     float speedWay = 5.0f;
     public Transform target;
-    // Use this for initialization
-    override void Start () {//TO DO: RECIBIR DEGENETIC MANAGER, 11 valores
+
+    Vector3 StartPosition;
+    Quaternion startRotation;
+    Rigidbody rb;
+    float[] raycasts = new float[5];
+    // Use t1his for initialization
+    protected override void Start () {//TO DO: RECIBIR DEGENETIC MANAGER, 11 valores
 
         maxSpeed = 0.28f;
         maxAcceleration = 0.15f;
@@ -19,27 +24,31 @@ public class WP_Actor : Movement {
     protected override float AccelerationAxis()
     {
         // -1 descelerar, 1 acelerar
+        if (raycasts[1] > raycasts[2] || raycasts[3] > raycasts[2])
+        {
+            return -1;
+        }
+        return 1;
     }
 
 
     protected override float SteerAxis()
     {
         // -1 izquierda, 1 derecha
+        if(raycasts[1] > raycasts[2])
+        {
+            if (raycasts[1] > raycasts[3])
+                return -1;
+            else return 1;
+        }
+        else if(raycasts[3] > raycasts[2])
+        {
+            return 1;
+        }
+        return 0;
     }
 
 
     // Update is called once per frame
-    void Update () {
-        transform.Translate(new Vector3(0, 0, speedWay * Time.deltaTime));
-        transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Waypoint")
-        {
-            target = other.gameObject.GetComponent<Waypoint>().nextPoint;
-            transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
-        }
-    }
 }
